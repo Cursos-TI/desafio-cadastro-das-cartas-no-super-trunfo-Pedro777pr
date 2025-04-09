312fc1ef115d5b31c746dde1316fb6cabc5cee81
Pedro777pr <pedroaraujofaria.pj29@gmail.com>
2025-04-09 18:32:20 +0000

    atualizei logica Super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+float calcular_densidade_p(int populacao, float area) {
+    if (area == 0) { 
+        return 0;
+    }
+    return populacao / area;
+}
+
+float calcular_pib_per_capita(float pib, int populacao) {
+    if (populacao == 0) { 
+        return 0;
+    }
+    return pib / populacao;
+}
+
 int main() {
     char Estado = 'A';
     char Codigo_da_Carta[20] = "A01";
@@ -9,7 +23,6 @@ int main() {
     float PIB = 699.28;
     int Numero_de_Pontos_Turisticos = 50;
 
-   
     printf("Carta 1:\n");
     printf("Estado: %c\n", Estado);
     printf("Codigo da Carta: %s\n", Codigo_da_Carta);
@@ -18,7 +31,7 @@ int main() {
     printf("Area: %.2f km2\n", Area);
     printf("PIB: %.2f bilhoes de reais\n", PIB);
     printf("Numero de Pontos Turisticos: %d\n", Numero_de_Pontos_Turisticos);
-    
+
     char Estado2 = 'B';
     char Codigo_da_Carta2[20] = "B02";
     char Nome_da_Cidade2[20] = "Rio de Janeiro";
@@ -35,4 +48,19 @@ int main() {
     printf("Area2: %.2f km2\n", Area2);
     printf("PIB2: %.2f bilhoes de reais\n", PIB2);
     printf("Numero de Pontos Turisticos2: %d\n", Numero_de_Pontos_Turisticos2);
+
+    float densidadep1 = calcular_densidade_p(Populacao, Area);
+    float pibpercapita1 = calcular_pib_per_capita(PIB, Populacao);
+    float densidadep2 = calcular_densidade_p(Populacao2, Area2);
+    float pibpercapita2 = calcular_pib_per_capita(PIB2, Populacao2);
+
+    printf("Densidade Populacional Carta 1: %.2f hab/km2\n", densidadep1);
+    printf("PIB per Capita Carta 1: 56.7367139959 bilhoes de reais\n", pibpercapita1);
+    printf("Densidade Populacional Carta 2: %.2f hab/km2\n", densidadep2);
+    printf("PIB per Capita Carta 2: 103.734439834 bilhoes de reais\n", pibpercapita2);
+
+    printf("Comparacao de Cartas:\n");
+    printf("Populacao: Carta %d venceu (%d)\n", 1 + (Populacao < Populacao2), (Populacao > Populacao2) * 1);
+
+    return 0;
 }
